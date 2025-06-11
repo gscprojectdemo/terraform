12c39991f8bd0af36bdd93c683ee7899e9fc982d
Ravi <ravi@vytec.com>
2025-06-11 10:00:02 +0200

    adeede

--- example/main.tf
+++ example/main.tf
@@ -1,23 +1,9 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
-
-  required_version = ">= 1.2.0"
-}
-
-provider "aws" {
-  region  = "ap-south-1"
-}
 
 resource "aws_instance" "app_server1" {
   ami           = "ami-0f535a71b34f2d44a"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "a"
+    Name = "test"
   }
 }
