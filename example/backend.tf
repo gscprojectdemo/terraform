793ff2fc720d0d1203075c2240b2d08a4b0dd7ae
Ravi <ravi@vytec.com>
2025-06-11 13:18:08 +0200

    updated

--- example/backend.tf
+++ example/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "terraformstatebbucket11062025"
-    key    = "tf/staging/terraform.tfstate"
+    key    = "tf/dev/terraform.tfstate"
     region = "ap-south-1"
     dynamodb_table = "tfstate-dynamodb-table" 
     #use_lockfile = true
--- example/main.tf
+++ example/main.tf
@@ -1,6 +1,22 @@
+data "aws_key_pair" "example" {
+  key_name           = "mykey"
+  include_public_key = true
+}
 
+output "fingerprint" {
+  value = data.aws_key_pair.example.fingerprint
+}
+
+output "name" {
+  value = data.aws_key_pair.example.key_name
+}
+
+output "id" {
+  value = data.aws_key_pair.example.id
+}
 resource "aws_instance" "app_server1" {
   ami           = var.imageid
+  key_name = data.aws_key_pair.example.key_name
   instance_type = var.instancetype
   subnet_id  = var.subnetid
   tags = {
