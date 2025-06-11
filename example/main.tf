d55fb404c05fe513ad97c1de3e444f528db82f93
Ravi <ravi@vytec.com>
2025-06-11 07:06:58 +0200

    updated

--- example/main.tf
+++ example/main.tf
@@ -21,3 +21,11 @@ resource "aws_instance" "app_server1" {
     Name = "ExampleAppServerInstance"
   }
 }
+resource "aws_instance" "app_server1" {
+  ami           = "ami-0f535a71b34f2d44a"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
